22eea232f634834270746265a93464ae6957beb1
Olivier Haley <129203990+tigerwoodsjr@users.noreply.github.com>
2024-03-15 12:15:00 -0600

    s

--- R/app_server.R
+++ R/app_server.R
@@ -8,7 +8,8 @@
 app_server <- function(input, output, session) {
   # plot stock data with plotly
   output$stock_plotly <- plotly::renderPlotly({
-    plotly::plot_ly(my_dataset, x = ~date, y = ~close, type = 'scatter', mode = 'lines', line = list(color = 'blue'))
+    plotly::plot_ly(my_dataset, x = ~date, y = ~close, type = 'scatter', mode = 'lines', line = list(color = 'blue')) %>%
+      plotly::layout(title = paste0(symbol, "Stock Price"), xaxis = list(title = ''), yaxis = list(title = 'Price'))
   })
 
   # Your application server logic
